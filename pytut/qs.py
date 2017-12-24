c0ee51f54c676a41362cb0e7a4c28ae72e48d408
Jack Currie <jackcurrie1@gmail.com>
2017-12-24 13:52:54 -0700

    initial commit

---
+++ pytut/qs.py
@@ -0,0 +1,12 @@
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + right
+
+print(quicksort([4, 4, 2,  5, 1, 8, 9, 9, 0, -12]))
